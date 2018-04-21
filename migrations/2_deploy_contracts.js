7cc73511a35d8210280990790443634a85811900
anhgien <giencntt@gmail.com>
2018-04-21 10:45:27 +0700

    logs creator address

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var ContractiumToken = artifacts.require("./ContractiumToken.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(ContractiumToken);
+module.exports = function(deployer, network, accounts) {
+  let account = accounts[0];
+  console.log("Account deploy ", account);
+  deployer.deploy(ContractiumToken, {from: account});
 };
