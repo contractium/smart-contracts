248ec41422ad5cababfafb060bcddd93cb36df1d
anhgien <giencntt@gmail.com>
2018-04-21 10:44:59 +0700

    set up deploy for test

--- truffle.js
+++ truffle.js
@@ -1,9 +1,22 @@
+const config = require('./configs/config.json');
+const HDWalletProvider = require("truffle-hdwallet-provider-privkey");
+
 module.exports = {
   networks: {
     development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
+      provider: function() {
+        const privKey = config.development.private_key;
+        return new HDWalletProvider(privKey, config.development.provider_url)
+      },
+      network_id: "0" // Match any network id
+    },    
+    ropsten: {
+      provider: function() {
+        const privKeys = config.ropsten.private_key;
+        return new HDWalletProvider(privKeys, config.ropsten.provider_url)
+      },
+      gas: 3000000,
+      network_id: "2" // Match any network id
     }
   }
 };
