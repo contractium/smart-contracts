8ff96ab307df3830d1dfcc659619044694d0e80f
Phúc Hồng <thphuc.le@gmail.com>
2018-06-13 15:11:42 +0700

    add contract spend token

---
+++ contracts/ContractSpendToken.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.4.21;
+
+import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+/**
+ * @title Contract Spend Token
+ * @dev Trusted contracts can spend this token
+ * @dev Manage list of trusted contract
+ * @dev each one has the destination address to receive the spender token
+ */
+contract ContractSpendToken is Ownable, StandardToken {
+    mapping (address => address) private contractToReceiver;
+
+    function addContract(address _contractAdd, address _to) external onlyOwner returns (bool) {
+        contractToReceiver[_contractAdd] = _to;
+        return true;
+    }
+
+    function removeContract(address _contractAdd) external onlyOwner returns (bool) {
+        contractToReceiver[_contractAdd] = address(0);
+        return true;
+    }
+
+    function contractSpend(address _from, uint256 _value) public returns (bool) {
+        address _to = contractToReceiver[msg.sender];
+        require(_to != address(0));
+        require(_value <= balances[_from]);
+
+        balances[_from] = balances[_from].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+}
